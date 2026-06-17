dba99a524002f716445983f5e7e745ff9133ae9a
wa oi <2324089471@qq.com>
2026-06-17 10:33:04 +0800

    Refactor Home.vue template and script structure

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -177,7 +177,7 @@ const handleResetConfig = () => {
   Object.assign(apiConfig, {
     provider: 'tongyi',
     apiKey: 'sk-96f45e8bc7ac4063bbb319494fcab27f',
-    model: 'qwen-turbo',
+    model: 'qwen3.7-plus',
     baseUrl: 'https://dashscope.aliyuncs.com'
   })
 }
